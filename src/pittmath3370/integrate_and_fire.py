a9ede332972a0c144beb88b4becd08a42a7ad788
Noah Bright <noah.bright.1@gmail.com>
2024-06-09 13:47:25 -0400

    put QIF in class

--- src/pittmath3370/integrate_and_fire.py
+++ src/pittmath3370/integrate_and_fire.py
@@ -1,8 +1,16 @@
 import neuronal_models as nm
+# TODO fix E
+# TODO Add other models
 
 
 class QIF(nm.NeuronalModel):
-    def dVdt(t, V, I, E):
+    def __init__(self):
+        super().__init__()
+        self.E = 1
+
+    @staticmethod
+    def dALLdt(t, V, self, clamp):
         # Define your ordinary differential equation here
-        dVdt = E - V + I(t)
-        return dVdt
+        if clamp:
+            return 0
+        return self.E - V + self.I0 + self.I_inj(t)
